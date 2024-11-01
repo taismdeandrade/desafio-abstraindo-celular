ac4e75f8a623348df2a8bde0cfcfa012d8f39f7d
TaisAndrade <taismdeandrade@gmail.com>
2024-11-01 10:17:36 -0300

    Subindo projeto

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("-----Nokia-----");
+Smartphone nokia = new Nokia("119823312", "Modelo Nokia", "111212", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+nokia.ReceberLigacao();
+
+Console.WriteLine("-----Iphone-----");
+Smartphone iphone = new Iphone("12923123", "Modelo Iphone", "22323232", 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
+iphone.ReceberLigacao();
